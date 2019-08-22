44cabc8bba3434cfdf66339bfc4c9e5846d76bd1
Leo <losoviz@gmail.com>
2019-08-22 13:50:49 +0800

    Renamed RequestCache to Memory

--- src/Facades/MemoryManagerFacade.php
+++ src/Facades/MemoryManagerFacade.php
@@ -4,10 +4,10 @@ namespace PoP\ComponentModel\Facades;
 use PoP\ComponentModel\Cache\CacheInterface;
 use PoP\Root\Container\ContainerBuilderFactory;
 
-class RequestCacheFacade
+class MemoryManagerFacade
 {
     public static function getInstance(): CacheInterface
     {
-        return ContainerBuilderFactory::getInstance()->get('request_cache');
+        return ContainerBuilderFactory::getInstance()->get('memory_cache');
     }
 }
--- src/Facades/MemoryManagerItemPoolFacade.php
+++ src/Facades/MemoryManagerItemPoolFacade.php
@@ -4,10 +4,10 @@ namespace PoP\ComponentModel\Facades;
 use Psr\Cache\CacheItemPoolInterface;
 use PoP\Root\Container\ContainerBuilderFactory;
 
-class RequestCacheItemPoolFacade
+class MemoryManagerItemPoolFacade
 {
     public static function getInstance(): CacheItemPoolInterface
     {
-        return ContainerBuilderFactory::getInstance()->get('request_cache_item_pool');
+        return ContainerBuilderFactory::getInstance()->get('memory_cache_item_pool');
     }
 }
