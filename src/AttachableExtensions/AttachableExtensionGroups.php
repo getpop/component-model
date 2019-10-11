e333bb1e62949e559bebe03d5eff80f625e936f6
Leo <losoviz@gmail.com>
2019-10-11 17:20:09 +0800

    Moved constants under ComponentModel

---
+++ src/AttachableExtensions/AttachableExtensionGroups.php
@@ -0,0 +1,9 @@
+<?php
+namespace PoP\ComponentModel\AttachableExtensions;
+
+class AttachableExtensionGroups
+{
+    const FIELDVALUERESOLVERS = 'fieldvalueresolvers';
+    const FIELDDIRECTIVERESOLVERS = 'fielddirectiveresolvers';
+    const FIELDRESOLVERPICKERS = 'fieldresolver-pickers';
+}
--- src/Container/ContainerBuilderUtils.php
+++ src/Container/ContainerBuilderUtils.php
@@ -1,7 +1,7 @@
 <?php
 namespace PoP\ComponentModel\Container;
 
-use PoP\ComponentModel\FieldValueResolvers\FieldValueResolverInterface;
+use PoP\ComponentModel\AttachableExtensions\AttachableExtensionGroups;
 use PoP\Root\Container\ContainerBuilderUtils as RootContainerBuilderUtils;
 
 class ContainerBuilderUtils {
@@ -15,10 +15,7 @@ class ContainerBuilderUtils {
     public static function attachFieldValueResolversFromNamespace(string $namespace): void
     {
         foreach (RootContainerBuilderUtils::getNamespaceServiceIds($namespace) as $serviceClass) {
-            // Make sure this class is a fieldValueResolver, which implements the Attachable trait
-            // if ($serviceClass::class instanceof FieldValueResolverInterface) {
-                $serviceClass::attach(POP_ATTACHABLEEXTENSIONGROUP_FIELDVALUERESOLVERS);
-            // }
+            $serviceClass::attach(AttachableExtensionGroups::FIELDVALUERESOLVERS);
         }
     }
 }
