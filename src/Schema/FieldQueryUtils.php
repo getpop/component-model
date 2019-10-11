47bfe186f6922d571e13d36a6e537d9be5081701
Leo <losoviz@gmail.com>
2019-10-11 19:23:47 +0800

    Extracted function to Utils class

---
+++ src/Schema/FieldQueryUtils.php
@@ -0,0 +1,21 @@
+<?php
+namespace PoP\ComponentModel\Schema;
+
+class FieldQueryUtils
+{
+    public static function isAnyFieldArgumentValueAField(array $fieldArgValues): bool
+    {
+        $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
+        $isOrContainsAField = array_map(
+            function($fieldArgValue) use($fieldQueryInterpreter) {
+                // Either the value is a field, or it is an array of fields
+                if (is_array($fieldArgValue)) {
+                    return self::isAnyFieldArgumentValueAField((array)$fieldArgValue);
+                }
+                return $fieldQueryInterpreter->isFieldArgumentValueAField($fieldArgValue);
+            },
+            $fieldArgValues
+        );
+        return (in_array(true, $isOrContainsAField));
+    }
+}
