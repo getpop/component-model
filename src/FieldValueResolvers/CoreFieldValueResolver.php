feb0cfafb58d734c3be019ceec544799e0727dca
Leo <losoviz@gmail.com>
2019-10-11 16:26:13 +0800

    Satisfy field "id" from within fieldValueResolver

---
+++ src/FieldValueResolvers/CoreFieldValueResolver.php
@@ -0,0 +1,59 @@
+<?php
+namespace PoP\ComponentModel\FieldValueResolvers;
+use \PoP\ComponentModel\FieldResolverBase;
+
+class CoreFieldValueResolver extends AbstractDBDataFieldValueResolver
+{
+    public static function getClassesToAttachTo(): array
+    {
+        return [
+            FieldResolverBase::class,
+        ];
+    }
+
+    public function getFieldNamesToResolve(): array
+    {
+        return [
+            'id',
+        ];
+    }
+
+    public function getFieldDocumentationType(string $fieldName): ?string
+    {
+        $types = [
+            'id' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName];
+    }
+
+    public function getFieldDocumentationDescription(string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+            'id' => $translationAPI->__('The DB Object ID', 'pop-component-model'),
+        ];
+        return $descriptions[$fieldName];
+    }
+
+    public function resolveValue($fieldResolver, $resultItem, string $fieldName, array $fieldArgs = [])
+    {
+        switch ($fieldName) {
+            case 'id':
+                return $fieldResolver->getId($resultItem);
+        }
+
+        return parent::resolveValue($fieldResolver, $resultItem, $fieldName, $fieldArgs);
+    }
+
+    public function resolveFieldDefaultDataloaderClass($fieldResolver, string $fieldName, array $fieldArgs = []): ?string
+    {
+        switch ($fieldName) {
+            case 'id':
+                return $fieldResolver->getIdFieldDataloaderClass();
+        }
+        return parent::resolveFieldDefaultDataloaderClass($fieldResolver, $fieldName, $fieldArgs);
+    }
+}
+
+// Static Initialization: Attach
+CoreFieldValueResolver::attach(POP_ATTACHABLEEXTENSIONGROUP_FIELDVALUERESOLVERS);
