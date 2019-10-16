f79561b6ac8be2a56a51b28284ff81c2e2c6832b
Leo <losoviz@gmail.com>
2019-10-16 13:25:30 +0800

    Removed unneeded import

--- src/ModuleProcessors/AbstractModuleProcessor.php
+++ src/ModuleProcessors/AbstractModuleProcessor.php
@@ -7,7 +7,6 @@ use PoP\ComponentModel\Facades\Managers\ModulePathHelpersFacade;
 use PoP\ComponentModel\Modules\ModuleUtils;
 use PoP\ComponentModel\ModuleFilters\ModulePaths;
 use PoP\ComponentModel\Facades\Managers\InstanceManagerFacade;
-use PoP\ComponentModel\Schema\QuerySyntax;
 use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
 use PoP\ComponentModel\Facades\Managers\ModuleProcessorManagerFacade;
 use PoP\ComponentModel\Engine_Vars;
