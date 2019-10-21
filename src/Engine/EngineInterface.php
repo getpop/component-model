0fad4fcc2c2f420f9906b08176fe2b3b2fe924ae
Leo <losoviz@gmail.com>
2019-10-21 17:03:19 +0800

    Added functions to interface

--- src/Engine/EngineInterface.php
+++ src/Engine/EngineInterface.php
@@ -3,5 +3,22 @@ namespace PoP\ComponentModel\Engine;
 
 interface EngineInterface
 {
-
+    public function getOutputData();
+    public function addBackgroundUrl($url, $targets);
+    public function getEntryModule(): array;
+    public function sendEtagHeader();
+    public function getExtraRoutes();
+    public function listExtraRouteVars();
+    public function generateData();
+    public function calculateOutuputData();
+    public function getModelPropsModuletree(array $module);
+    public function addRequestPropsModuletree(array $module, array $props);
+    public function getModuleDatasetSettings(array $module, $model_props, array &$props);
+    public function getRequestMeta();
+    public function getSessionMeta();
+    public function getSiteMeta();
+    public function validateCheckpoints($checkpoints);
+    public function getModuleData($root_module, $root_model_props, $root_props);
+    public function moveEntriesUnderDBName(array $entries, bool $entryHasId, $dataloader): array;
+    public function getDatabases();
 }
