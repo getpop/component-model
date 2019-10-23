dd89b6fe63acec2ccd95b6546336ccf7a5104bad
Leo <losoviz@gmail.com>
2019-10-23 22:52:44 +0800

    Show dbError if DB object is null

--- src/DirectiveResolvers/ResolveValueAndMergeDirectiveResolver.php
+++ src/DirectiveResolvers/ResolveValueAndMergeDirectiveResolver.php
@@ -1,9 +1,10 @@
 <?php
 namespace PoP\ComponentModel\DirectiveResolvers;
+use PoP\ComponentModel\GeneralUtils;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
 use PoP\ComponentModel\Facades\Schema\FeedbackMessageStoreFacade;
 use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
-use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
-use PoP\ComponentModel\GeneralUtils;
 
 class ResolveValueAndMergeDirectiveResolver extends AbstractDirectiveResolver
 {
@@ -22,10 +23,20 @@ class ResolveValueAndMergeDirectiveResolver extends AbstractDirectiveResolver
 
     protected function resolveValueForResultItems(FieldResolverInterface $fieldResolver, array &$resultIDItems, array &$idsDataFields, array &$dbItems, array &$dbErrors, array &$dbWarnings, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations)
     {
+        $translationAPI = TranslationAPIFacade::getInstance();
         $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
         foreach (array_keys($idsDataFields) as $id) {
             // Obtain its ID and the required data-fields for that ID
             $resultItem = $resultIDItems[$id];
+            // It could be that the object is NULL. For instance: a post has a location stored a meta value, and the corresponding location object was deleted, so the ID is pointing to a non-existing object
+            // In that case, simply return a dbError
+            if (is_null($resultItem)) {
+                $dbErrors[(string)$id]['id'][] = sprintf(
+                    $translationAPI->__('Corrupted data: Object with ID \'%s\' doesn\'t exist', 'component-model'),
+                    $id
+                );
+                continue;
+            }
             // $conditionalResultIDItems = [$id => $resultItem];
             $this->resolveValuesForResultItem($fieldResolver, $id, $resultItem, $idsDataFields[(string)$id]['direct'], $dbItems, $dbErrors, $dbWarnings);
 
