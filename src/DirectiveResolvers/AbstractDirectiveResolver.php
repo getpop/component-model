a36a9aa3c5ec76009fb62b04d9a9dd7de1c02b18
Leo <losoviz@gmail.com>
2019-10-27 15:31:52 +0800

    Introduced `getFieldNamesToApplyTo` to directives

--- src/DirectiveResolvers/AbstractDirectiveResolver.php
+++ src/DirectiveResolvers/AbstractDirectiveResolver.php
@@ -1,9 +1,11 @@
 <?php
 namespace PoP\ComponentModel\DirectiveResolvers;
 use League\Pipeline\StageInterface;
-use PoP\ComponentModel\DirectivePipeline\DirectivePipelineUtils;
-use PoP\ComponentModel\AttachableExtensions\AttachableExtensionTrait;
 use PoP\ComponentModel\FieldResolvers\AbstractFieldResolver;
+use PoP\ComponentModel\DirectivePipeline\DirectivePipelineUtils;
+use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
+use PoP\ComponentModel\AttachableExtensions\AttachableExtensionTrait;
+use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
 
 abstract class AbstractDirectiveResolver implements DirectiveResolverInterface, StageInterface
 {
@@ -22,6 +24,18 @@ abstract class AbstractDirectiveResolver implements DirectiveResolverInterface, 
         ];
     }
 
+    /**
+     * Indicate to what fieldNames this directive can be applied.
+     * Returning an empty array means all of them
+     *
+     * @return array
+     */
+    public static function getFieldNamesToApplyTo(): array
+    {
+        // By default, apply to all fieldNames
+        return [];
+    }
+
     public function __invoke($payload)
     {
         // 1. Extract the arguments from the payload
@@ -37,7 +51,20 @@ abstract class AbstractDirectiveResolver implements DirectiveResolverInterface, 
             $schemaDeprecations
         ) = DirectivePipelineUtils::extractArgumentsFromPayload($payload);
 
-        // 2. Execute operation
+        // 2. Validate operation
+        $this->validateDirective(
+            $fieldResolver,
+            $resultIDItems,
+            $idsDataFields,
+            $dbItems,
+            $dbErrors,
+            $dbWarnings,
+            $schemaErrors,
+            $schemaWarnings,
+            $schemaDeprecations
+        );
+
+        // 3. Execute operation
         $this->resolveDirective(
             $fieldResolver,
             $resultIDItems,
@@ -50,7 +77,7 @@ abstract class AbstractDirectiveResolver implements DirectiveResolverInterface, 
             $schemaDeprecations
         );
 
-        // 3. Re-create the payload from the modified variables
+        // 4. Re-create the payload from the modified variables
         return DirectivePipelineUtils::convertArgumentsToPayload(
             $fieldResolver,
             $resultIDItems,
@@ -62,5 +89,60 @@ abstract class AbstractDirectiveResolver implements DirectiveResolverInterface, 
             $schemaWarnings,
             $schemaDeprecations
         );
+    }
+
+    public function validateDirective(FieldResolverInterface $fieldResolver, array &$resultIDItems, array &$idsDataFields, array &$dbItems, array &$dbErrors, array &$dbWarnings, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations)
+    {
+        // Check that the directive can be applied to all provided fields
+        $this->validateAndFilterFieldsForDirective($idsDataFields, $schemaWarnings);
+    }
+
+    /**
+     * Check that the directive can be applied to all provided fields
+     *
+     * @param array $idsDataFields
+     * @param array $schemaWarnings
+     * @return void
+     */
+    protected function validateAndFilterFieldsForDirective(array &$idsDataFields, array &$schemaWarnings)
+    {
+        $directiveSupportedFieldNames = $this->getFieldNamesToApplyTo();
+
+        // If this function returns an empty array, then it supports all fields, then do nothing
+        if (!$directiveSupportedFieldNames) {
+            return;
+        }
+
+        // Check if all fields are supported by this directive
+        $failedDataFields = [];
+        foreach ($idsDataFields as $id => $data_fields) {
+            // If any fieldName failed, remove it from the list of fields to execute for this directive
+            if ($unsupportedFieldNames = array_diff($data_fields['direct'], $directiveSupportedFieldNames)) {
+                $data_fields['direct'] = array_diff(
+                    $data_fields['direct'],
+                    $unsupportedFieldNames
+                );
+                $failedDataFields = array_values(array_unique(array_merge(
+                    $failedDataFields,
+                    $unsupportedFieldNames
+                )));
+            }
+        }
+        // Give a warning message for all failed fields
+        if ($failedDataFields) {
+            $translationAPI = TranslationAPIFacade::getInstance();
+            $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
+            $directiveName = $this->getDirectiveName();
+            $failedDataFieldOutputKeys = array_map(
+                [$fieldQueryInterpreter, 'getFieldOutputKey'],
+                $failedDataFields
+            );
+            $schemaWarnings[$directiveName][] = sprintf(
+                $translationAPI->__('Directive \'%s\' cannot support the following field(s), so it has not been executed on them: \'%s\'. (The only supported fields are: \'%s\')', 'component-model'),
+                $directiveName,
+                implode($translationAPI->__('\', \''), $failedDataFieldOutputKeys),
+                implode($translationAPI->__('\', \''), $directiveSupportedFieldNames)
+            );
+        }
     }
 }
--- src/DirectiveResolvers/DirectiveResolverInterface.php
+++ src/DirectiveResolvers/DirectiveResolverInterface.php
@@ -5,5 +5,27 @@ use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
 interface DirectiveResolverInterface
 {
     public static function getDirectiveName(): string;
+    /**
+     * Indicate to what fieldNames this directive can be applied.
+     * Returning an empty array means all of them
+     *
+     * @return array
+     */
+    public static function getFieldNamesToApplyTo(): array;
+    /**
+     * Validate that the directive can be applied to all passed fields
+     *
+     * @param FieldResolverInterface $fieldResolver
+     * @param array $resultIDItems
+     * @param array $idsDataFields
+     * @param array $dbItems
+     * @param array $dbErrors
+     * @param array $dbWarnings
+     * @param array $schemaErrors
+     * @param array $schemaWarnings
+     * @param array $schemaDeprecations
+     * @return void
+     */
+    public function validateDirective(FieldResolverInterface $fieldResolver, array &$resultIDItems, array &$idsDataFields, array &$dbItems, array &$dbErrors, array &$dbWarnings, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations);
     public function resolveDirective(FieldResolverInterface $fieldResolver, array &$resultIDItems, array &$idsDataFields, array &$dbItems, array &$dbErrors, array &$dbWarnings, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations);
 }
