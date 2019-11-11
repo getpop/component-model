60501eba88a9262996034b99bb3ea3e90cc87d7d
Leo <losoviz@gmail.com>
2019-11-11 22:59:02 +0800

    Split function into current/other types

--- src/Schema/TypeCastingHelpers.php
+++ src/Schema/TypeCastingHelpers.php
@@ -7,17 +7,30 @@ class TypeCastingHelpers
         return implode(':', $types);
     }
     /**
-     * If the type is a combination of 2 or more types, then return an array containing these element types
-     * Otherwise return null
+     * Return the current type combination element, which is simply the first element, always
+     * Eg: if passing "string", it is "string"; for "array:string", it is "array";
      *
      * @param [type] $type
      * @return void
      */
-    public static function maybeGetTypeCombinationElements($type): ?array {
+    public static function getTypeCombinationCurrentElement(string $type): string {
+        $maybeCombinationElems = explode(':', $type);
+        return $maybeCombinationElems[0];
+    }
+    /**
+     * If the type is a combination of 2 or more types, then return the string containing all of them except the first one
+     * Eg: if passing "string", it is null; for "array:string", it is "string"; for "array:array:string", it is "array:string"
+     *
+     * @param [type] $type
+     * @return void
+     */
+    public static function getTypeCombinationNestedElements(string $type): ?string {
         $maybeCombinationElems = explode(':', $type);
         if (count($maybeCombinationElems) >= 2) {
-            return $maybeCombinationElems;
+            // Remove the first element
+            return substr($type, strlen($maybeCombinationElems[0])+1);
         }
+        // There are no others
         return null;
     }
 }
