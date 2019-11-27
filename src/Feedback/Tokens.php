734b66cc5de8fc7432c277cf1fa9f1403e3fbdb0
Leo <losoviz@gmail.com>
2019-11-27 16:22:35 +0800

    Renamed class

---
+++ src/Feedback/Tokens.php
@@ -0,0 +1,7 @@
+<?php
+namespace PoP\ComponentModel\Feedback;
+
+class Tokens {
+    const PATH = 'path';
+    const MESSAGE = 'message';
+}
--- src/Schema/ResponseTokens.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace PoP\ComponentModel\Schema;
-
-class ResponseTokens {
-    const PATH_SEPARATOR = ' => ';
-    const PATH = 'path';
-    const MESSAGE = 'message';
-}
