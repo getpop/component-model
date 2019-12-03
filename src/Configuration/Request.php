e91140b90210dcea701346188fbc729b683c2441
Leo <losoviz@gmail.com>
2019-12-03 11:45:44 +0800

    Fixed bug that prevented installation

--- src/Configuration/Request.php
+++ src/Configuration/Request.php
@@ -11,7 +11,7 @@ class Request
         // By default, it is mangled, if not mangled then param "mangled" must have value "none"
         // Coment Leo 13/01/2017: getVars() can't function properly since it references objects which have not been initialized yet,
         // when called at the very beginning. So then access the request directly
-        return !$_REQUEST[self::URLPARAM_MANGLED] || $_REQUEST[self::URLPARAM_MANGLED] != self::URLPARAMVALUE_MANGLED_NONE;
+        return !isset($_REQUEST[self::URLPARAM_MANGLED]) || !$_REQUEST[self::URLPARAM_MANGLED] || $_REQUEST[self::URLPARAM_MANGLED] != self::URLPARAMVALUE_MANGLED_NONE;
     }
 }
 
