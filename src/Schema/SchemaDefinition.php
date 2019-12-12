877e3272f36f1191b7b27d0a27fae4de965fdbd1
Leo <losoviz@gmail.com>
2019-12-12 14:30:39 +0800

    Introduced arg "shape" to field "__schema"

--- src/Schema/SchemaDefinition.php
+++ src/Schema/SchemaDefinition.php
@@ -30,6 +30,9 @@ class SchemaDefinition {
     const ARGNAME_DIRECTIVE_LIMITED_TO_FIELDS = 'limitedToFields';
     const ARGNAME_DIRECTIVE_EXPRESSIONS = 'expressions';
 
+    const ARGVALUE_SCHEMA_SHAPE_FLAT = 'flat';
+    const ARGVALUE_SCHEMA_SHAPE_NESTED = 'nested';
+
     // Field/Directive Argument Types
     const TYPE_MIXED = 'mixed';
     const TYPE_ID = 'id';
