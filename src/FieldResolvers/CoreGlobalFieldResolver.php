82daddd94d2b258973202f5b45da4cc36c4cca5a
Leo <losoviz@gmail.com>
2019-12-17 02:08:52 +0800

    Added field "isInstanceOfType", made class global

--- src/FieldResolvers/CoreGlobalFieldResolver.php
+++ src/FieldResolvers/CoreGlobalFieldResolver.php
@@ -1,25 +1,20 @@
 <?php
 namespace PoP\ComponentModel\FieldResolvers;
-use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
+
+use PoP\ComponentModel\FieldResolvers\AbstractGlobalFieldResolver;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\Translation\Facades\TranslationAPIFacade;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 
-class CoreFieldResolver extends AbstractDBDataFieldResolver
+class CoreGlobalFieldResolver extends AbstractGlobalFieldResolver
 {
-    public static function getClassesToAttachTo(): array
-    {
-        return [
-            AbstractTypeResolver::class,
-        ];
-    }
-
     public static function getFieldNamesToResolve(): array
     {
         return [
             'id',
             'self',
             '__typename',
+            'isInstanceOfType',
         ];
     }
 
@@ -29,6 +24,7 @@ class CoreFieldResolver extends AbstractDBDataFieldResolver
             'id' => SchemaDefinition::TYPE_ID,
             'self' => SchemaDefinition::TYPE_ID,
             '__typename' => SchemaDefinition::TYPE_STRING,
+            'isInstanceOfType' => SchemaDefinition::TYPE_BOOL,
         ];
         return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
     }
@@ -40,8 +36,27 @@ class CoreFieldResolver extends AbstractDBDataFieldResolver
             'id' => $translationAPI->__('The object ID', 'pop-component-model'),
             'self' => $translationAPI->__('The same object', 'pop-component-model'),
             '__typename' => $translationAPI->__('The object\'s type', 'pop-component-model'),
+            'isInstanceOfType' => $translationAPI->__('Indicate if the object\'s is of a given type', 'pop-component-model'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'isInstanceOfType':
+                return [
+                    [
+                        SchemaDefinition::ARGNAME_NAME => 'type',
+                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
+                        SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The type name to compare against', 'component-model'),
+                        SchemaDefinition::ARGNAME_MANDATORY => true,
+                    ],
+                ];
+        }
+
+        return parent::getSchemaFieldArgs($typeResolver, $fieldName);
     }
 
     public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
@@ -52,6 +67,9 @@ class CoreFieldResolver extends AbstractDBDataFieldResolver
                 return $typeResolver->getId($resultItem);
             case '__typename':
                 return $typeResolver->getTypeName();
+            case 'isInstanceOfType':
+                $typeName = $fieldArgs['type'];
+                return strtolower($typeName) == strtolower($typeResolver->getTypeName());
         }
 
         return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
