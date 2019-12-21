f02707117e3af2043d0dd01f180681b9641d72a2
Leo <losoviz@gmail.com>
2019-12-21 17:02:49 +0800

    Created UnionTypeDataLoader

---
+++ src/TypeDataLoaders/UnionTypeDataLoader.php
@@ -0,0 +1,10 @@
+<?php
+namespace PoP\ComponentModel\TypeDataLoaders;
+
+class UnionTypeDataLoader extends AbstractTypeDataLoader
+{
+    public function getObjects(array $ids): array
+    {
+        return [];
+    }
+}
