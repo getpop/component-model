78b7dc8d8b27b57e93b937eac993cebd49398895
Leo <losoviz@gmail.com>
2020-02-22 15:47:51 +0800

    Moved code into trait

--- src/FieldResolvers/AbstractGlobalFieldResolver.php
+++ src/FieldResolvers/AbstractGlobalFieldResolver.php
@@ -1,20 +1,9 @@
 <?php
 namespace PoP\ComponentModel\FieldResolvers;
 
-use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
-use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\ComponentModel\FieldResolvers\GlobalFieldResolverTrait;
 
 abstract class AbstractGlobalFieldResolver extends AbstractDBDataFieldResolver
 {
-    public static function getClassesToAttachTo(): array
-    {
-        return [
-        	AbstractTypeResolver::class,
-        ];
-    }
-
-    public function isGlobal(TypeResolverInterface $typeResolver, string $fieldName): bool
-    {
-        return true;
-    }
+    use GlobalFieldResolverTrait;
 }
---
+++ src/FieldResolvers/GlobalFieldResolverTrait.php
@@ -0,0 +1,20 @@
+<?php
+namespace PoP\ComponentModel\FieldResolvers;
+
+use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+
+trait GlobalFieldResolverTrait
+{
+    public static function getClassesToAttachTo(): array
+    {
+        return [
+        	AbstractTypeResolver::class,
+        ];
+    }
+
+    public function isGlobal(TypeResolverInterface $typeResolver, string $fieldName): bool
+    {
+        return true;
+    }
+}
