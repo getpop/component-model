cb873e3754e5e7d88cec19291b5e6ba0c5664de0
Leo <losoviz@gmail.com>
2020-02-22 17:53:11 +0800

    Re-enabled needsIDsDataFieldsToExecute

--- src/Schema/SchemaDefinition.php
+++ src/Schema/SchemaDefinition.php
@@ -32,7 +32,7 @@ class SchemaDefinition {
     const ARGNAME_GLOBAL_DIRECTIVES = 'globalDirectives';
     const ARGNAME_DIRECTIVE_PIPELINE_POSITION = 'pipelinePosition';
     const ARGNAME_DIRECTIVE_CAN_EXECUTE_MULTIPLE_TIMES = 'canExecuteMultipleTimes';
-    // const ARGNAME_DIRECTIVE_NEEDS_DATA_TO_EXECUTE = 'needsDataToExecute';
+    const ARGNAME_DIRECTIVE_NEEDS_DATA_TO_EXECUTE = 'needsDataToExecute';
     const ARGNAME_DIRECTIVE_LIMITED_TO_FIELDS = 'limitedToFields';
     const ARGNAME_DIRECTIVE_EXPRESSIONS = 'expressions';
 
