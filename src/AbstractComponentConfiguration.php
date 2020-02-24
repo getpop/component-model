e4d24c947b68a6e7ddbb24e6bed6fd3b2468a189
Leo <losoviz@gmail.com>
2020-02-24 15:22:43 +0800

    Created component configuration class

---
+++ src/AbstractComponentConfiguration.php
@@ -0,0 +1,33 @@
+<?php
+namespace PoP\ComponentModel;
+
+use PoP\Hooks\Facades\HooksAPIFacade;
+
+abstract class AbstractComponentConfiguration
+{
+    protected static $initialized = [];
+    public static function getHookName(string $class, string $envVariable): string
+    {
+        return sprintf(
+            '%s:configuration:%s',
+            $class,
+            $envVariable
+        );
+    }
+    protected static function maybeInitEnvironmentVariable(string $envVariable, &$selfProperty, callable $callback): void
+    {
+        if (!self::$initialized[$envVariable]) {
+            self::$initialized[$envVariable] = true;
+            $hooksAPI = HooksAPIFacade::getInstance();
+            $hookName = self::getHookName(
+                get_called_class(),
+                $envVariable
+            );
+            $selfProperty = $hooksAPI->applyFilters(
+                $hookName,
+                $callback
+            );
+        }
+    }
+}
+
