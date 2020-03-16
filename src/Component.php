633fa273279cf298ac223e0809dcae698234ed05
Leo <losoviz@gmail.com>
2020-03-17 04:41:39 +0800

    Renamed `boot` to `prematureBoot`

--- src/Component.php
+++ src/Component.php
@@ -29,9 +29,9 @@ class Component extends AbstractComponent
      *
      * @return void
      */
-    public static function boot()
+    public static function prematureBoot()
     {
-        parent::boot();
+        parent::prematureBoot();
 
         // Initialize classes
         ContainerBuilderUtils::attachFieldResolversFromNamespace(__NAMESPACE__.'\\FieldResolvers');
