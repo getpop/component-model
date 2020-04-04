71b37eb0642edeb32cac4136909edbe9e6bddbeb
Leo <losoviz@gmail.com>
2020-04-04 17:56:00 +0800

    Moved function to Routing package

--- src/Misc/RequestUtils.php
+++ src/Misc/RequestUtils.php
@@ -91,20 +91,6 @@ class RequestUtils
         return urldecode($url);
     }
 
-    public static function getURLPath()
-    {
-        // Allow to remove the language information from qTranslate (https://domain.com/en/...)
-        $route = HooksAPIFacade::getInstance()->applyFilters(
-            '\PoP\Routing:uri-route',
-            $_SERVER['REQUEST_URI']
-        );
-        $params_pos = strpos($route, '?');
-        if ($params_pos !== false) {
-            $route = substr($route, 0, $params_pos);
-        }
-        return trim($route, '/');
-    }
-
     public static function getFramecomponentModules()
     {
         return HooksAPIFacade::getInstance()->applyFilters(
