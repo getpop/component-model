0caf823e1c57031b5554457edd43b4b9290d868e
Tan Choon Tick <cttan@Tans-MacBook-Air.local>
2020-05-08 19:41:47 +0800

    Added missing function to Interface

--- src/Engine/DataloadingEngineInterface.php
+++ src/Engine/DataloadingEngineInterface.php
@@ -8,6 +8,7 @@ interface DataloadingEngineInterface
 {
     public function getMandatoryDirectiveClasses(): array;
     public function getMandatoryDirectives(): array;
+    public function addMandatoryDirective(string $directive): void;
     public function addMandatoryDirectiveClass(string $directiveClass): void;
     public function addMandatoryDirectiveClasses(array $directiveClasses): void;
 }
