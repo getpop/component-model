a2c8aec1d13af1164fa92f0acc195a92bb8d401d
Leonardo Losoviz <leo@getpop.org>
2020-07-14 08:19:02 +0800

    An enum can have a core value (in addition to the value)

--- src/Enums/EnumInterface.php
+++ src/Enums/EnumInterface.php
@@ -12,4 +12,6 @@ interface EnumInterface
     public function getNamespace(): string;
     public function getNamespacedName(): string;
     public function getMaybeNamespacedName(): string;
+    public function getCoreValues(): ?array;
+    public function getCoreValue(string $enumValue): ?string;
 }
