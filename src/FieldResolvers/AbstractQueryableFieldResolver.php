3b9c96efa539f9856bc98b215e8538d90c4b2116
Leonardo Losoviz <leo@getpop.org>
2020-07-16 21:02:29 +0800

    Extracted code to extract schema definition from filter to trait

--- src/FieldResolvers/AbstractQueryableFieldResolver.php
+++ src/FieldResolvers/AbstractQueryableFieldResolver.php
@@ -4,26 +4,23 @@ declare(strict_types=1);
 
 namespace PoP\ComponentModel\FieldResolvers;
 
-use PoP\ComponentModel\Misc\GeneralUtils;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\ComponentModel\Resolvers\QueryableFieldResolverTrait;
 use PoP\ComponentModel\Facades\Instances\InstanceManagerFacade;
 use PoP\ComponentModel\TypeDataLoaders\TypeQueryableDataLoaderInterface;
-use PoP\ComponentModel\Facades\ModuleProcessors\ModuleProcessorManagerFacade;
 
 abstract class AbstractQueryableFieldResolver extends AbstractDBDataFieldResolver
 {
+    use QueryableFieldResolverTrait;
+
     protected function getFieldArgumentsSchemaDefinitions(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): array
     {
         $schemaDefinitions = parent::getFieldArgumentsSchemaDefinitions($typeResolver, $fieldName, $fieldArgs);
 
         if ($filterDataloadingModule = $this->getFieldDefaultFilterDataloadingModule($typeResolver, $fieldName, $fieldArgs)) {
-            $moduleprocessor_manager = ModuleProcessorManagerFacade::getInstance();
-            $filterqueryargs_modules = $moduleprocessor_manager->getProcessor((array)$filterDataloadingModule)->getDataloadQueryArgsFilteringModules($filterDataloadingModule);
             $schemaDefinitions = array_merge(
                 $schemaDefinitions,
-                GeneralUtils::arrayFlatten(array_map(function ($module) use ($moduleprocessor_manager) {
-                    return $moduleprocessor_manager->getProcessor($module)->getFilterInputSchemaDefinitionItems($module);
-                }, $filterqueryargs_modules))
+                $this->getFilterSchemaDefinitionItems($filterDataloadingModule)
             );
         }
 
