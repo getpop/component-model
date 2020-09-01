1fad579f520eccc5b3f2aafeebeb46dff2b359f4
Leonardo Losoviz <leo@getpop.org>
2020-09-02 06:43:27 +0800

    Use generics as annotations

--- src/Component.php
+++ src/Component.php
@@ -40,7 +40,7 @@ class Component extends AbstractComponent
     /**
      * Initialize services
      *
-     * @param mixed[] $configuration
+     * @param array<string, mixed> $configuration
      * @param string[] $skipSchemaComponentClasses
      */
     protected static function doInitialize(
