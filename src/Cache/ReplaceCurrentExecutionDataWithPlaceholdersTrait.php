292e5c60563fd6214015ee308e283560a602d13d
Leonardo Losoviz <leo@getpop.org>
2020-12-17 19:08:37 +0800

    Fixed replacing content in multidimensional array for PHP 8.0

--- src/Cache/ReplaceCurrentExecutionDataWithPlaceholdersTrait.php
+++ src/Cache/ReplaceCurrentExecutionDataWithPlaceholdersTrait.php
@@ -37,9 +37,26 @@ trait ReplaceCurrentExecutionDataWithPlaceholdersTrait
         // Replace the placeholder for the uniqueId with the current uniqueId
         // Do the same with all dynamic constants, so that we can generate a proper ETag also when retrieving the cached value
         $replacements = $this->getCacheReplacements();
+        $replaceFrom = array_values($replacements);
+        $replaceTo = array_keys($replacements);
+        if (is_array($content)) {
+            /**
+             * A faster way to replace the strings in multidimensional array
+             * is to json_encode() it, do the str_replace() and then json_decode() it
+             * @see https://www.php.net/manual/en/function.str-replace.php#100871
+             */
+            return json_decode(
+                str_replace(
+                    $replaceFrom,
+                    $replaceTo,
+                    json_encode($content)
+                ),
+                true
+            );
+        }
         return str_replace(
-            array_values($replacements),
-            array_keys($replacements),
+            $replaceFrom,
+            $replaceTo,
             $content
         );
     }
