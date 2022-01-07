16d7e03b9ac78bfe97de895f58fcaf95341d4d15
leoloso <leo@getpop.org>
2022-01-07 01:36:53 +0000

    Merge pull request #1336 from leoloso/Have-FunctionFields-change-dependency-from-Engine-to-ComponentModel
    Have FunctionFields change dependency from Engine to ComponentModel

---
+++ src/TypeResolvers/ScalarType/AnyBuiltInScalarScalarTypeResolver.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\TypeResolvers\ScalarType;
+
+use PoP\ComponentModel\TypeResolvers\ScalarType\AbstractScalarTypeResolver;
+use stdClass;
+
+class AnyBuiltInScalarScalarTypeResolver extends AbstractScalarTypeResolver
+{
+    public function getTypeName(): string
+    {
+        return 'AnyBuiltInScalar';
+    }
+
+    public function getTypeDescription(): ?string
+    {
+        return $this->__('Wildcard type representing any of GraphQL\'s built-in types (String, Int, Boolean, Float or ID)', 'engine');
+    }
+
+    /**
+     * Accept anything and everything, other than arrays and objects
+     */
+    public function coerceValue(string|int|float|bool|stdClass $inputValue): string|int|float|bool|object
+    {
+        if ($error = $this->validateIsNotStdClass($inputValue)) {
+            return $error;
+        }
+        return $inputValue;
+    }
+}
