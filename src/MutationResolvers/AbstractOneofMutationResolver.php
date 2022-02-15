54564c6c4121a2e9915c3dde4de1ed135f6e3e23
leoloso <leo@getpop.org>
2022-02-15 07:36:15 +0000

    Fixed PHPStan

--- src/MutationResolvers/AbstractOneofMutationResolver.php
+++ src/MutationResolvers/AbstractOneofMutationResolver.php
@@ -122,6 +122,9 @@ abstract class AbstractOneofMutationResolver extends AbstractMutationResolver
         return key($formData);
     }
 
+    /**
+     * @throws QueryResolutionException
+     */
     final public function executeMutation(array $formData): mixed
     {
         [$inputFieldMutationResolver, $inputFieldFormData] = $this->getInputFieldMutationResolverAndFormData($formData);
@@ -129,8 +132,13 @@ abstract class AbstractOneofMutationResolver extends AbstractMutationResolver
     }
     final public function validateErrors(array $formData): array
     {
-        [$inputFieldMutationResolver, $inputFieldFormData] = $this->getInputFieldMutationResolverAndFormData($formData);
-        return $inputFieldMutationResolver->validateErrors((array)$inputFieldFormData);
+        try {
+            [$inputFieldMutationResolver, $inputFieldFormData] = $this->getInputFieldMutationResolverAndFormData($formData);
+            return $inputFieldMutationResolver->validateErrors((array)$inputFieldFormData);
+        } catch (QueryResolutionException $e) {
+            // Return the error message from the exception
+            return [$e->getMessage()];
+        }
     }
     final public function validateWarnings(array $formData): array
     {
@@ -138,6 +146,7 @@ abstract class AbstractOneofMutationResolver extends AbstractMutationResolver
             [$inputFieldMutationResolver, $inputFieldFormData] = $this->getInputFieldMutationResolverAndFormData($formData);
             return $inputFieldMutationResolver->validateWarnings((array)$inputFieldFormData);
         } catch (QueryResolutionException $e) {
+            // Do nothing since the Error will already return the problem
             return [];
         }
     }
