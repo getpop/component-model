f4946fb50e68985e284a4f2facbc6833e2a48aab
leoloso <leo@getpop.org>
2022-04-06 12:19:49 +0000

    Merge pull request #1507 from leoloso/Add-BrainFaker-to-mock-WordPress-code-on-PHPUnit-tests
    Add BrainFaker to mock WordPress objects and functions on PHPUnit tests

---
+++ src/App/AbstractComponentModelAppProxy.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\ComponentModel\App;
+
+use PoP\ComponentModel\App as ComponentModelApp;
+use PoP\ComponentModel\AppInterface as ComponentModelAppInterface;
+use PoP\ComponentModel\Engine\EngineState;
+use PoP\ComponentModel\Feedback\FeedbackStore;
+use PoP\ComponentModel\Stores\MutationResolutionStoreInterface;
+use PoP\ComponentModel\Tracing\TracingStore;
+use PoP\Root\App\AbstractRootAppProxy;
+
+/**
+ * Using proxy instead of inheritance, so that the upstream App
+ * class is still the single source of truth for its own state
+ */
+abstract class AbstractComponentModelAppProxy extends AbstractRootAppProxy implements ComponentModelAppInterface
+{
+    public static function getEngineState(): EngineState
+    {
+        return ComponentModelApp::getEngineState();
+    }
+
+    public static function getFeedbackStore(): FeedbackStore
+    {
+        return ComponentModelApp::getFeedbackStore();
+    }
+
+    public static function getTracingStore(): TracingStore
+    {
+        return ComponentModelApp::getTracingStore();
+    }
+
+    public static function getMutationResolutionStore(): MutationResolutionStoreInterface
+    {
+        return ComponentModelApp::getMutationResolutionStore();
+    }
+
+    public static function regenerateEngineState(): void
+    {
+        ComponentModelApp::regenerateEngineState();
+    }
+
+    public static function regenerateFeedbackStore(): void
+    {
+        ComponentModelApp::regenerateFeedbackStore();
+    }
+
+    public static function regenerateTracingStore(): void
+    {
+        ComponentModelApp::regenerateTracingStore();
+    }
+
+    public static function regenerateMutationResolutionStore(): void
+    {
+        ComponentModelApp::regenerateMutationResolutionStore();
+    }
+}
